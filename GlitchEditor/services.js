6f8e2071b4a2b3940f297f52e21398eda1da09ae
David Le <43181047+ShadowVaddix@users.noreply.github.com>
2021-03-07 23:46:11 -0600

    Add files via upload

--- GlitchEditor/services.js
+++ GlitchEditor/services.js
@@ -3,17 +3,13 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+function myUser() {
+    var person = document.getElementById('myUsername').value
+    var password = document.getElementById('myPassword').value
+    if (person != "" && password !="") {
+        alert("Welcome " + person + "! Good to see you today!");
+    } else
+    {
+        alert("Please login to GlitchEditor or face the consequences.")
     }
 }
